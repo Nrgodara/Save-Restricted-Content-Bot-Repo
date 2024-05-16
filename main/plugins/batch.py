956bc4e3fd7553a3716c0aaa661a97ab0a0db002
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-05-16 12:15:04 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -1,4 +1,4 @@
-#Join t.me/dev_gagan
+# Join t.me/dev_gagan
 
 import logging
 import time, os, asyncio
@@ -13,7 +13,7 @@ from main.plugins.helpers import get_link, screenshot
 from telethon import events, Button, errors
 from telethon.tl.types import DocumentAttributeVideo
 
-from pyrogram import Client 
+from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 
 logging.basicConfig(level=logging.DEBUG,
@@ -25,13 +25,14 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 batch = []
 ids = []
+cancel_flag = {}
 
 '''async def get_pvt_content(event, chat, id):
     msg = await userbot.get_messages(chat, ids=id)
     await event.client.send_message(event.chat_id, msg) 
 '''
 
-@gagan.on(events.NewMessage(incoming=True, pattern='/batch')& filters.user(AUTH))
+@gagan.on(events.NewMessage(incoming=True, pattern='/batch') & filters.user(AUTH))
 async def _batch(event):
     s = False
     if f'{event.sender_id}' in batch:
@@ -46,7 +47,6 @@ async def _batch(event):
                 except Exception:
                     await conv.send_message("No link found.")
             except Exception as e:
-                #print(e)
                 logger.info(e)
                 return await conv.send_message("Cannot wait more longer for your response!")
             await conv.send_message(f"Send me the number of files/range you want to save from the given message, as a reply to this message.", buttons=Button.force_reply())
@@ -54,7 +54,6 @@ async def _batch(event):
                 _range = await conv.get_reply()
             except Exception as e:
                 logger.info(e)
-                #print(e)
                 return await conv.send_message("Cannot wait more longer for your response!")
             try:
                 value = int(_range.text)
@@ -69,6 +68,7 @@ async def _batch(event):
                 await conv.send_message(r)
                 return
             batch.append(f'{event.sender_id}')
+            cancel_flag[event.sender_id] = False
             cd = await conv.send_message("**Batch process ongoing...**\n\nProcess completed: ", 
                                     buttons=[[Button.url("Developer", url="http://t.me/mr_mahiji")]])
             co = await run_batch(userbot, Bot, event.sender_id, cd, _link) 
@@ -81,15 +81,19 @@ async def _batch(event):
             conv.cancel()
             ids.clear()
             batch.clear()
+            del cancel_flag[event.sender_id]
 
 @gagan.on(events.callbackquery.CallbackQuery(data="cancel"))
 async def cancel(event):
-    ids.clear()
-    batch.clear()
+    user_id = event.sender_id
+    cancel_flag[user_id] = True
+    await event.respond("Batch process has been cancelled.")
 
-    
 async def run_batch(userbot, client, sender, countdown, link):
     for i in range(len(ids)):
+        if cancel_flag.get(sender):
+            await client.send_message(sender, "Batch process has been cancelled.")
+            return
         timer = 6
         if i < 250:
             timer = 2
@@ -110,7 +114,6 @@ async def run_batch(userbot, client, sender, countdown, link):
             timer = 1 if i < 500 else 2
         try: 
             count_down = f"**⚡Batch process ongoing⚡**\n\n**🍁Process completed:** `{i+1}`"
-            #a =ids[i]
             try:
                 msg_id = int(link.split("/")[-1])
             except ValueError:
@@ -120,11 +123,9 @@ async def run_batch(userbot, client, sender, countdown, link):
                 msg_id = int(link_.split("/")[-1])
             integer = msg_id + int(ids[i])
             await get_bulk_msg(userbot, client, sender, link, integer)
-            #protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
             await countdown.edit(count_down, 
                                  buttons=[[Button.url("Join Channel", url="https://t.me/mahi_batches")]])
             await asyncio.sleep(timer)
-            #await protection.delete()
         except IndexError as ie:
             await client.send_message(sender, f" {i}  {ie}  \n\n💚**Batch completed!**💚")
             await countdown.delete()
@@ -142,12 +143,10 @@ async def run_batch(userbot, client, sender, countdown, link):
                 try:
                     await get_bulk_msg(userbot, client, sender, link, integer)
                 except Exception as e:
-                    #print(e)
                     logger.info(e)
                     if countdown.text != count_down:
                         await countdown.edit(count_down, buttons=[[Button.url("Join Channel", url="http://t.me/mahi_batches")]])
         except Exception as e:
-            #print(e)
             logger.info(e)
             await client.send_message(sender, f"An error occurred during cloning, batch will continue.\n\n**Error:** {str(e)}")
             if countdown.text != count_down:
@@ -176,6 +175,7 @@ async def start_command(event):
         caption=TEXT,
         buttons=buttons
     )
+           
             
 TEXTING = """
 ```
