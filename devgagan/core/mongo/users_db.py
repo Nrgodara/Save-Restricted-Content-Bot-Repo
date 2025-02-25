aa7e1f8aab49d3193362aa4cc9782448dd543481
MAHI® <155721977+Nrgodara@users.noreply.github.com>
2025-02-25 13:54:09 +0530

    Update users_db.py

--- devgagan/core/mongo/users_db.py
+++ devgagan/core/mongo/users_db.py
@@ -51,4 +51,26 @@ async def del_user(user):
     await db.users.delete_one({"user": user})
     
 
+async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+    
+    async def ban_user(self, user_id, ban_reason="No Reason"):
+        ban_status = dict(
+            is_banned=True,
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
 
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id':int(id)})
+        if not user:
+            return default
+        return user.get('ban_status', default)
