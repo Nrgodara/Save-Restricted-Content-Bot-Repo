90413d3fbb31b87c700dfadb656dcd2006de53d9
MAHI® <155721977+Nrgodara@users.noreply.github.com>
2025-03-01 17:57:37 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -167,8 +167,8 @@ async def smart_handler(client, message):
         await message.reply(
             "Click the button below to verify your free access token:\n\n"
             "> What will you get ?\n"
-            "1. No time bound upto 3 hours\n"
-            "2. **Become Premium user for 3 hours"
+            "1. No time Gap BTW links\n"
+            "2. **Become Premium user for 3 hours**"
             "3. All functions unlocked\n\n"
             "[ʜᴏᴡ ᴛᴏ ᴠᴇʀɪғʏ❓](https://t.me/Mahi_Bots/23250)", 
             reply_markup=button
